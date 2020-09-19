68eedd246e3510fe20cae5d8c07770bae34387c3
Tom <wonggamto@163.com>
2020-09-19 18:12:14 +0800

    初始化vue-router

---
+++ src/components/demo2.vue
@@ -0,0 +1,5 @@
+<template>
+    <div>
+        demo2
+    </div>
+</template>
---
+++ src/shims-vue.d.ts
@@ -0,0 +1,5 @@
+declare module '*.vue'{
+  import {Component} from 'vue'
+  const componentOptions:Component
+  export default componentOptions
+}
