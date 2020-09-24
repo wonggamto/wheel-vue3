7fdeac926380708f1cc3c6fd247ecb3fa5e3294a
Tom <wonggamto@163.com>
2020-09-24 23:26:31 +0800

    调整 topnav 和 aside 的位置

--- src/components/Topnav.vue
+++ src/components/Topnav.vue
@@ -15,6 +15,8 @@
         background: #59af7d;
         display: flex;
         padding: 16px;
+        position: relative;
+        z-index: 10;
         > .logo{
             max-width: 6em;
             margin-right: auto;
