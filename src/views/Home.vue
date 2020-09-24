d62eff51cfab40ee4628277471783164f8e4a4da
Tom <wonggamto@163.com>
2020-09-24 21:15:49 +0800

    搭建HTML结构

---
+++ src/views/Home.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <div class="topNav">
+            <div class="logo"></div>
+            <div class="menu"></div>
+        </div>
+        <div class="banner">
+            <h1>极 UI</h1>
+            <h2>一个简单的 易上手的 UI 框架</h2>
+            <p class="actions">
+                <a href="">GitHub</a>
+                <a href="">开始</a>
+            </p>
+        </div>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+    .topNav{
+        border:1px solid red;
+    }
+</style>
