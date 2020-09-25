6aa5684bce9978928e5ea218f012cb9399de6807
Tom <wonggamto@163.com>
2020-09-25 14:31:45 +0800

    优化switch css代码

--- src/libs/Switch.vue
+++ src/libs/Switch.vue
@@ -8,9 +8,9 @@
     props: {
       value: Boolean
     },
-    setup(props,context) {
+    setup(props, context) {
       const toggle = () => {
-        context.emit('update:value',!props.value)
+        context.emit('update:value', !props.value);
       };
       return {toggle};
     }
@@ -26,34 +26,43 @@
         background: #bfbfbf;
         border-radius: $h/2;
         position: relative;
+
+        > span {
+            position: absolute;
+            top: 2px;
+            left: 2px;
+            height: $h2;
+            width: $h2;
+            background: white;
+            border-radius: $h2 / 2;
+            transition: all 250ms;
+        }
+
+        &.checked {
+            background: #1890ff;
+
+            > span {
+                left: calc(100% - #{$h2} - 2px);
+            }
+        }
+
+        &:focus {
+            outline: none;
+        }
+
+        &:active {
+            > span {
+                width: $h2 + 4px;
+            }
+        }
+
+        &.checked:active {
+            > span {
+                width: $h2 + 4px;
+                margin-left: -4px;
+            }
+        }
     }
 
-    span {
-        position: absolute;
-        top: 2px;
-        left: 2px;
-        height: $h2;
-        width: $h2;
-        background: white;
-        border-radius: $h2 / 2;
-        transition: all 250ms;
-    }
 
-    button.checked {
-        background: #1890ff;
-    }
-
-    button.checked > span {
-        left: calc(100% - #{$h2} - 2px);
-    }
-
-    button:focus {
-        outline: none;
-    }
-    button:active{
-        > span{width:$h2 + 4px;}
-    }
-    button.checked:active{
-        > span{width:$h2 + 4px;margin-left: -4px;}
-    }
 </style>
