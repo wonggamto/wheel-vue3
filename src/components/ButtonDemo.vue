6cbce9898a29ce0940e1b789ca33118a798335b5
Tom <wonggamto@163.com>
2020-09-25 17:31:45 +0800

    支持size属性

--- src/components/ButtonDemo.vue
+++ src/components/ButtonDemo.vue
@@ -1,11 +1,18 @@
 <template>
     <div>button</div>
-    <h1>Demo</h1>
+    <h1>Demo1</h1>
     <div>
         <Button>普通</Button>
-        <Button theme="button">按钮</Button>
+        <Button theme="button">普通</Button>
         <Button theme="link">链接</Button>
         <Button theme="text">文本</Button>
+
+    </div>
+    <h1>Demo2</h1>
+    <div>
+        <Button>普通</Button>
+        <Button size="big">Big</Button>
+        <Button size="small">Small</Button>
     </div>
 </template>
 <script lang="ts">
--- src/libs/Button.vue
+++ src/libs/Button.vue
@@ -1,27 +1,43 @@
 <template>
-    <button class="g-button" :class="{[`g-theme-${theme}`]:theme}">
+    <button class="g-button" :class="classes">
         <slot/>
     </button>
 </template>
 <script lang="ts">
+  import {computed} from 'vue';
+
   export default {
     props: {
       theme: {
-        type:String,
-        default:'button'
+        type: String,
+        default: 'button'
+      },
+      size: {
+        type: String,
+        default: 'normal',
       }
+    },
+    setup(props) {
+      const {theme, size} = props;
+      const classes = computed(() => {
+        return {
+          [`g-theme-${theme}`]: theme,
+          [`g-size-${size}`]: size
+        };
+      });
+      return {classes};
     }
   };
 </script>
 <style lang="scss">
-    $h:32px;
-    $border-color:#d9d9d9;
-    $color:#333;
-    $blue:#40a9ff;
-    $radius:4px;
-    .g-button{
+    $h: 32px;
+    $border-color: #d9d9d9;
+    $color: #333;
+    $blue: #40a9ff;
+    $radius: 4px;
+    .g-button {
         box-sizing: border-box;
-        height:$h;
+        height: $h;
         padding: 0 12px;
         cursor: pointer;
         display: inline-block;
@@ -29,31 +45,51 @@
         white-space: nowrap;
         background: white;
         color: $color;
-        border:1px solid $border-color;
+        border: 1px solid $border-color;
         border-radius: $radius;
-        box-shadow: 0 1px 0 fade-out(black,0.95);
-        & + &{
+        box-shadow: 0 1px 0 fade-out(black, 0.95);
+
+        & + & {
             margin-left: 8px;
         }
+
         &:hover,
-        &:focus{
-            color:$blue;
+        &:focus {
+            color: $blue;
         }
-        &:focus{
+
+        &:focus {
             outline: none;
         }
-        &::-moz-focus-inner{
-            border:0;
+
+        &::-moz-focus-inner {
+            border: 0;
         }
-        &.g-theme-link{
-            border-color:transparent;
-            box-shadow:none;
-            color:$blue;
-            &:hover,&:focus{
-                color:lighten($blue,10%);
+
+        &.g-theme-link {
+            border-color: transparent;
+            box-shadow: none;
+            color: $blue;
+
+            &:hover, &:focus {
+                color: lighten($blue, 10%);
             }
-            &:hover,&:focus{
-                background: darken(white,5%);
+
+            &:hover, &:focus {
+                background: darken(white, 5%);
+            }
+        }
+
+        &.g-theme-button {
+                &.g-size-big {
+                    font-size: 24px;
+                    height: 48px;
+                    padding:0 16px;
+            }
+            &.g-size-small{
+                font-size:12px;
+                height:20px;
+                padding:0 4px;
             }
         }
     }
