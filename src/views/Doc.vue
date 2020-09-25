e6dabeb101ae4e2b549baa75e3f5a755444d1e75
Tom <wonggamto@163.com>
2020-09-25 10:32:02 +0800

    完善aside切换逻辑

--- src/views/Doc.vue
+++ src/views/Doc.vue
@@ -40,11 +40,6 @@
         background: lightgreen;
         width: 150px;
         padding: 16px;
-        position: fixed;
-        top: 0;
-        left: 0;
-        padding-top: 70px;
-
         > h2 {
             margin-bottom: 4px;
         }
@@ -53,6 +48,12 @@
             > li {
                 padding: 4px 0;
             }
+        }
+        @media (max-width: 500px) {
+        position: fixed;
+        top: 0;
+        left: 0;
+        padding-top: 70px;
         }
     }
 </style>
