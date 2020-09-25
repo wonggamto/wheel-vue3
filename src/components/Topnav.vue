4cb2c2656866da9c967eda86f6a79726d6633e09
Tom <wonggamto@163.com>
2020-09-25 10:25:21 +0800

    topNav响应式，添加toggleAside样式

--- src/components/Topnav.vue
+++ src/components/Topnav.vue
@@ -5,6 +5,7 @@
             <li>菜单1</li>
             <li>菜单2</li>
         </ul>
+        <span class="toggleAside"></span>
     </div>
 </template>
 <script lang="ts">
@@ -27,7 +28,8 @@
         padding: 16px;
         position: relative;
         z-index: 10;
-
+        justify-content: center;
+        align-items: center;
         > .logo {
             max-width: 6em;
             margin-right: auto;
@@ -41,6 +43,22 @@
             > li {
                 margin: 0 1em;
             }
+        }
+        .toggleAside{
+            display: none;
+            width: 24px;
+            height: 24px;
+            background: red;
+            position: absolute;
+            left: 16px;
+            top:50%;
+            transform: translateY(-50%);
+
+        }
+        @media (max-width: 500px) {
+            > .menu{display: none;}
+            > .logo{margin: 0 auto;}
+            > .toggleAside{display: inline-block;}
         }
     }
 </style>
