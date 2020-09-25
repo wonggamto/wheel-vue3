de4376af1755662f8ebffa9d058cfa569dbfcb2c
Tom <wonggamto@163.com>
2020-09-25 22:56:30 +0800

    button支持level

--- src/components/ButtonDemo.vue
+++ src/components/ButtonDemo.vue
@@ -24,6 +24,24 @@
             <Button theme="text">text</Button>
             <Button size="small" theme="text">Small-Text</Button>
         </div>
+        <h1>Demo3</h1>
+        <div>
+            <div>
+                <Button level="main">Main</Button>
+                <Button>Normal</Button>
+                <Button level="danger">Danger</Button>
+            </div>
+            <div>
+                <Button level="main" theme="link">Main-Link</Button>
+                <Button theme="link">Normal-Link</Button>
+                <Button level="danger" theme="link">Danger-Link</Button>
+            </div>
+            <div>
+                <Button level="main" theme="text">Main</Button>
+                <Button theme="text">Normal-Text</Button>
+                <Button level="danger" theme="text">Danger-text</Button>
+            </div>
+        </div>
     </div>
 </template>
 <script lang="ts">
--- src/libs/Button.vue
+++ src/libs/Button.vue
@@ -15,14 +15,19 @@
       size: {
         type: String,
         default: 'normal',
+      },
+      level:{
+        type:String,
+        default:"normal",
       }
     },
     setup(props) {
-      const {theme, size} = props;
+      const {theme, size, level} = props;
       const classes = computed(() => {
         return {
           [`g-theme-${theme}`]: theme,
-          [`g-size-${size}`]: size
+          [`g-size-${size}`]: size,
+          [`g-level-${level}`]: level
         };
       });
       return {classes};
@@ -35,6 +40,7 @@
     $color: #333;
     $blue: #40a9ff;
     $radius: 4px;
+    $red: red;
     .g-button {
         box-sizing: border-box;
         height: $h;
@@ -48,6 +54,7 @@
         border: 1px solid $border-color;
         border-radius: $radius;
         box-shadow: 0 1px 0 fade-out(black, 0.95);
+        transition: background 250ms;
 
         & + & {
             margin-left: 8px;
@@ -75,6 +82,12 @@
                 color: lighten($blue, 10%);
             }
 
+        }
+
+        &.g-theme-text {
+            border-color: transparent;
+            box-shadow: none;
+
             &:hover, &:focus {
                 background: darken(white, 5%);
             }
@@ -84,6 +97,7 @@
             font-size: 24px;
             height: 48px;
             padding: 0 16px;
+            color: inherit;
         }
 
         &.g-size-small {
@@ -92,5 +106,61 @@
             padding: 0 4px;
         }
 
+        &.g-theme-button {
+            &.g-level-main {
+                background: $blue;
+                color: white;
+                border-color: $blue;
+
+                &:hover,
+                &:focus {
+                    background: darken($blue, 10%);
+                    border-color: darken($blue, 10%);
+                }
+            }
+
+            &.g-level-danger {
+                background: $red;
+                border-color: $red;
+                color: white;
+
+                &:hover,
+                &:focus {
+                    background: darken($red, 10%);
+                    border-color: darken($red, 10%);
+                }
+            }
+        }
+
+        &.g-theme-link {
+            &.g-level-danger {
+                color: $red;
+
+                &:hover,
+                &:focus {
+                    color: darken($red, 10%);
+                }
+            }
+        }
+
+        &.g-theme-text {
+            &.g-level-main {
+                color: $blue;
+
+                &:hover,
+                &:focus {
+                    color: darken($blue, 10%);
+                }
+            }
+
+            &.g-level-danger {
+                color: $red;
+
+                &:hover,
+                &:focus {
+                    color: darken($red, 10%)
+                }
+            }
+        }
     }
 </style>
