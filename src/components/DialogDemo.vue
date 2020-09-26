463e5449bd1626f7bc1069c735358251be20b05f
Tom <wonggamto@163.com>
2020-09-26 15:16:16 +0800

    添加visible属性

--- src/components/DialogDemo.vue
+++ src/components/DialogDemo.vue
@@ -1,11 +1,22 @@
 <template>
-    <Dialog></Dialog>
+    <Button @click="toggle">toggle</Button>
+    <Dialog :visible="x"></Dialog>
 </template>
 <script lang="ts">
   import Dialog from '../libs/Dialog.vue';
+  import Button from '../libs/Button.vue';
+  import {ref} from 'vue'
   export default {
     components:{
-      Dialog
+      Button,
+      Dialog,
+    },
+    setup(){
+      const x = ref(false)
+      const toggle = ()=>{
+        x.value = !x.value
+      }
+      return {x,toggle}
     }
   }
 </script>
