a3da839f5685e941f6606af31269a9620912df57
Tom <wonggamto@163.com>
2020-09-28 21:17:10 +0800

    Tabs css1

--- src/libs/Tabs.vue
+++ src/libs/Tabs.vue
@@ -1,7 +1,11 @@
 <template>
-    <div>
-        <div v-for="(t,index) in titles" :key="index">{{t}}</div>
-        <component v-for="(c,index) in defaults" :is="c" :key="index"/>
+    <div class="g-tabs">
+        <div class=" g-tabs-nav">
+            <div v-for="(t,index) in titles" :key="index">{{t}}</div>
+        </div>
+        <div class="g-tabs-content">
+            <component v-for="(c,index) in defaults" :is="c" :key="index"/>
+        </div>
     </div>
 </template>
 <script lang="ts">
@@ -24,3 +28,29 @@
     }
   };
 </script>
+<style lang="scss">
+    $blue:#40a9ff;
+    $color:#333;
+    $border-color:#d9d9d9;
+    .g-tabs{
+        &-nav{
+            display: flex;
+            color:$color;
+            border-bottom: 1px solid $border-color;
+            &-item{
+                padding: 8px 0;
+                margin: 0 16px;
+                cursor:pointer;
+                &:first-child{
+                    margin-left: 0;
+                }
+                &.selected{
+                    color:$blue;
+                }
+            }
+        }
+        &-content{
+            padding:8px 0;
+        }
+    }
+</style>
