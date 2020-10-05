0639cc34e153f9925b80e0ca6c104fe32cc6ac83
Tom <wonggamto@163.com>
2020-10-05 13:59:56 +0800

    引入github样式

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import './libs/g.scss';
 import './index.scss';
 import App from './App.vue';
 import {router} from './router';
+import 'github-markdown-css';
 
 const app = createApp(App);
 app.use(router);
