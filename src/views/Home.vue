d825b13d83488201ab7569311580c03de42897c8
Tom <wonggamto@163.com>
2020-10-05 12:04:09 +0800

    clip-path 添加圆弧

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -44,6 +44,7 @@
     .topnavAndBanner {
         background: rgb(227, 255, 253);
         background: linear-gradient(145deg, rgba(227, 255, 253, 1) 0%, rgba(183, 233, 230, 1) 100%);
+        clip-path: ellipse(80% 60% at 50% 40%);
     }
 
     .banner {
