bc27f0fcb7f2bc4d7f4602a00f2d2e2c264a2462
Tom <wonggamto@163.com>
2020-10-05 13:03:29 +0800

    features 样式

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -12,15 +12,29 @@
             </div>
         </div>
         <div class="features">
-            <svg class="icon">
-                <use xlink:href="#icon-Vue"></use>
-            </svg>
-            <svg class="icon">
-                <use xlink:href="#icon-typescript"></use>
-            </svg>
-            <svg class="icon">
-                <use xlink:href="#icon-light"></use>
-            </svg>
+            <ul>
+                <li>
+                    <svg class="icon">
+                        <use xlink:href="#icon-Vue"></use>
+                    </svg>
+                    <h3>基于Vue 3</h3>
+                    <p>使用了 Vue 3 的UI组件库</p>
+                </li>
+                <li>
+                    <svg class="icon">
+                        <use xlink:href="#icon-typescript"></use>
+                    </svg>
+                    <h3>基于 TypeScript</h3>
+                    <p>源代码采用 TypeScrip 书写</p>
+                </li>
+                <li>
+                    <svg class="icon">
+                        <use xlink:href="#icon-light"></use>
+                    </svg>
+                    <h3>代码易读</h3>
+                    <p>每个组件的源代码都极其简洁</p>
+                </li>
+            </ul>
         </div>
     </div>
 </template>
@@ -35,12 +49,49 @@
     $green: #02bcb0;
     $border-radius: 4px;
     $color: #007974;
-    .features{
-        > svg{
-            width: 64px;
-            height: 64px;
+    .features {
+        margin: 64px auto;
+        width: 400px;
+
+        @media (min-width: 800px) {
+            width: 800px;
+        }
+        @media (min-width: 1200px) {
+            width: 1200px;
+        }
+
+        > ul {
+            display: flex;
+            flex-wrap: wrap;
+
+            > li {
+                width: 400px;
+                margin: 16px 0;
+                display: grid;
+                justify-content: start;
+                align-content: space-between;
+                grid-template-areas: "icon title" "icon text";
+                grid-template-columns: 80px auto;
+                grid-template-rows: 1fr auto;
+
+                > svg {
+                    grid-area: icon;
+                    width: 64px;
+                    height: 64px;
+                }
+
+                > h3 {
+                    grid-area: title;
+                    font-size: 28px;
+                }
+                > p{
+                    grid-area: text;
+                }
+            }
+
         }
     }
+
     .topnavAndBanner {
         background: rgb(227, 255, 253);
         background: linear-gradient(145deg, rgba(227, 255, 253, 1) 0%, rgba(183, 233, 230, 1) 100%);
