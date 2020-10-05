c48050ee66347daf536c9d4a08bb1ac2161bff28
Tom <wonggamto@163.com>
2020-10-05 13:15:41 +0800

    更新切换按钮的样式

--- src/components/Topnav.vue
+++ src/components/Topnav.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="topNav">
-        <div class="logo" >
-            <svg class="icon" >
+        <div class="logo">
+            <svg class="icon">
                 <use xlink:href="#icon-gg"></use>
             </svg>
         </div>
@@ -10,13 +10,21 @@
                 <router-link to="/doc">文档</router-link>
             </li>
         </ul>
-        <span class="toggleAside" @click="toggleMenu"></span>
+        <svg v-if="toggleMenuButtonVisible" class="toggleAside" @click="toggleMenu">
+            <use xlink:href="#icon-menu"></use>
+        </svg>
     </div>
 </template>
 <script lang="ts">
   import {inject, Ref} from 'vue';
 
   export default {
+    props: {
+      toggleMenuButtonVisible: {
+        type: Boolean,
+        default: false
+      }
+    },
     setup() {
       const asideVisible = inject<Ref<boolean>>('asideVisible');
       const toggleMenu = () => {
@@ -27,22 +35,24 @@
   };
 </script>
 <style lang="scss" scoped>
-    $color:#007974;
+    $color: #007974;
     .topNav {
         color: $color;
         display: flex;
         padding: 16px;
         position: fixed;
-        top:0;
-        left:0;
-        width:100%;
+        top: 0;
+        left: 0;
+        width: 100%;
         z-index: 10;
         justify-content: center;
         align-items: center;
+
         > .logo {
             max-width: 6em;
             margin-right: auto;
-            > svg{
+
+            > svg {
                 height: 32px;
                 width: 32px;
             }
@@ -57,21 +67,28 @@
                 margin: 0 1em;
             }
         }
-        .toggleAside{
+
+        .toggleAside {
             display: none;
-            width: 24px;
-            height: 24px;
-            background: red;
+            width: 32px;
+            height: 32px;
             position: absolute;
             left: 16px;
-            top:50%;
+            top: 50%;
             transform: translateY(-50%);
-
+            background:fade_out(black,0.9);
         }
+
         @media (max-width: 500px) {
-            > .menu{display: none;}
-            > .logo{margin: 0 auto;}
-            > .toggleAside{display: inline-block;}
+            > .menu {
+                display: none;
+            }
+            > .logo {
+                margin: 0 auto;
+            }
+            > .toggleAside {
+                display: inline-block;
+            }
         }
     }
 </style>
