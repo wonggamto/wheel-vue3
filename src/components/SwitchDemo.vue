07c1a8acca45b35a7892adf374e11eabc5fb28c5
Tom <wonggamto@163.com>
2020-10-05 16:58:28 +0800

    抽离用户需要拷贝的代码

--- src/components/SwitchDemo.vue
+++ src/components/SwitchDemo.vue
@@ -1,16 +1,77 @@
 <template>
-    <div>
-        <Switch v-model:value="bool"/>
+    <h1>Switch 组件示例</h1>
+    <div class="demo">
+        <h2>常规用法</h2>
+        <div class="demo-component">
+            <Switch1Demo/>
+        </div>
+        <div class="demo-actions">
+            <Button>查看代码</Button>
+        </div>
+        <div class="demo-code">
+            <pre>&lt;Switch v-model:value="bool" /&gt;</pre>
+        </div>
+    </div>
+    <div class="demo">
+        <h2>支持 disabled</h2>
+        <div class="demo-component">
+            <Switch2Demo/>
+        </div>
+        <div class="demo-actions">
+            <Button>查看代码</Button>
+        </div>
+        <div class="demo-code">
+            <pre>&lt;Switch v-model:value="bool" disabled /&gt;</pre>
+        </div>
     </div>
 </template>
 <script lang="ts">
   import Switch from '../libs/Switch.vue';
-  import {ref} from 'vue'
+  import Button from '../libs/Button.vue';
+  import {ref} from 'vue';
+  import Switch1Demo from './Switch1.demo.vue';
+  import Switch2Demo from './Switch2.demo.vue';
+
   export default {
-    components: {Switch},
-    setup(){
-      const bool = ref(false)
-      return {bool}
+    components: {
+      Switch2Demo,
+      Switch1Demo,
+      Switch,
+      Button
     },
+    setup() {
+      const bool = ref(false);
+      return {
+        bool
+      };
+    }
   };
 </script>
+<style lang="scss" scoped>
+    $border-color: #d9d9d9;
+    .demo {
+        border: 1px solid $border-color;
+        margin: 16px 0 32px;
+
+        > h2 {
+            font-size: 20px;
+            padding: 8px 16px;
+            border-bottom: 1px solid $border-color;
+        }
+
+        &-component {
+            padding: 16px;
+        }
+
+        &-actions {
+            padding: 8px 16px;
+            border-top: 1px dashed $border-color;
+
+            > pre {
+                line-height: 1.1;
+                font-family: Consolas, 'Courier New', Courier, monospace;
+                margin: 0;
+            }
+        }
+    }
+</style>
